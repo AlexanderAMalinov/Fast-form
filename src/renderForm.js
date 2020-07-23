0409e5d692627e2e77830722b9f753bb2c64d021
Alexander Malinov <alieksandr.malinov@mail.ru>
2020-07-23 16:48:32 +0300

    change structure of buildForm

--- src/renderForm.js
+++ src/renderForm.js
@@ -4,15 +4,20 @@ import getUserData from './getUserData.js';
 const questions = [];
 const questionsToStr = (arr) => JSON.stringify(arr.map((question) => JSON.stringify(question)));
 
+const buildForm = async (data) => {
+  getUserData(data)
+  .then(() => fs.appendFile('./form.js', ''))
+  .then(() => fs.writeFile('form.js', `import inquirer from 'inquirer';`))
+  .then(() => fs.appendFile('form.js', `\nconst form = ${questionsToStr(questions)};
+  const parsedForm = [];
+  form.reduce((acc, item) => {
+    const parsedQuestion = JSON.parse(item);
+    acc.push(parsedQuestion);
+    return acc;
+  }, parsedForm);`))
+  .then(() => fs.appendFile('form.js', `\ninquirer.prompt(parsedForm);`))
+  .then(() => console.log('Form was built!'));
+};
 
-getUserData(questions)
-.then(() => fs.appendFile('./form.js', ''))
-.then(() => fs.writeFile('form.js', `import inquirer from 'inquirer';`))
-.then(() => fs.appendFile('form.js', `\nconst form = ${questionsToStr(questions)};
-const parsedForm = [];
-form.reduce((acc, item) => {
-  const parsedQuestion = JSON.parse(item);
-  acc.push(parsedQuestion);
-  return acc;
-}, parsedForm);`))
-.then(() => fs.appendFile('form.js', `\ninquirer.prompt(parsedForm);`));
+buildForm(questions);
+
