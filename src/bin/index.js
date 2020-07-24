0c3cd61caeb2a4d7b0273ecbf731aa2e034ece27
Alexander Malinov <alieksandr.malinov@mail.ru>
2020-07-24 10:13:51 +0300

    update render and pull bin-file

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,1 +1,4 @@
 #!/usr/bin/env node
+import buildForm from '../buildForm.js';
+
+buildForm();
--- src/buildForm.js
+++ src/buildForm.js
@@ -1,11 +1,10 @@
 import fs from 'fs/promises';
 import getUserData from './getUserData.js';
 
-const questions = [];
-const questionsToStr = (arr) => JSON.stringify(arr.map((question) => JSON.stringify(question)));
-
-const buildForm = async (data) => {
-  getUserData(data)
+const buildForm = async () => {
+  const questions = [];
+  const questionsToStr = (arr) => JSON.stringify(arr.map((question) => JSON.stringify(question)));
+  getUserData(questions)
     .then(() => fs.appendFile('./form.js', ''))
     .then(() => fs.writeFile('form.js', 'import inquirer from \'inquirer\';'))
     .then(() => fs.appendFile('form.js', `\nconst form = ${questionsToStr(questions)};
@@ -16,7 +15,7 @@ const buildForm = async (data) => {
     return acc;
   }, parsedForm);`))
     .then(() => fs.appendFile('form.js', '\ninquirer.prompt(parsedForm);'))
-    .then(() => console.log('Form was built!'));
+    .then(() => console.log('\nForm was built!'));
 };
 
-buildForm(questions);
+export default buildForm;
