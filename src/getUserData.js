e46e23fbc8fb5e7989efa36a96f2745a01cab1d2
Alexander Malinov <alieksandr.malinov@mail.ru>
2020-07-25 13:31:43 +0300

    refactor getUserData and transfer responsibility for GenerateOptions in single module

--- src/getUserData.js
+++ src/getUserData.js
@@ -1,24 +1,21 @@
 import inquirer from 'inquirer';
 import Question from './question.js';
+import generateOptions from './generateOptions.js';
 
-// All texts for conversation with user
 const talk = {
-  greetings: () => {
-    console.log('\x1b[1m', 'Welcome to Fast-test!');
-    console.log('\x1b[1m', 'Let`s begin building your form!\n');
-  },
+  greetings: [
+    'Welcome to Fast-forms!',
+    'Let`s begin building your form!\n',
+  ],
   giveMeQuestion: 'Tell me your question:',
-  giveNextOption: 'Write option for your question here:',
   giveMeType: 'Choose type of answer(s) for your question',
 };
 
 const getUserData = async (arr) => {
   console.clear();
-  talk.greetings();
-  let indexOfQuestion = 1;
-  let flowMode = 'next';
-
-  while (flowMode === 'next') {
+  talk.greetings.forEach((words) => console.log('\x1b[1m', words));
+  let mode = true;
+  for (let indexOfQuestion = 1; mode; indexOfQuestion +=1) {
     const userQuestion = await inquirer.prompt({
       type: 'input',
       name: 'question',
@@ -30,40 +27,22 @@ const getUserData = async (arr) => {
       message: talk.giveMeType,
       choices: ['input', 'list'],
     });
-    const options = [];
 
-    // Generate options for list-question
-    if (typeOfUserQuestion.typeOfQuestion === 'list') {
-      let listMode = 'continue';
-      while (listMode === 'continue') {
-        const option = await inquirer.prompt({
-          type: 'input',
-          name: 'question',
-          message: talk.giveNextOption,
-        });
-        options.push(option.question);
-        listMode = await inquirer.prompt({
-          type: 'input',
-          name: 'question',
-          message: 'Continue? (y|n)',
-        });
-        listMode = listMode.question === 'y' ? 'continue' : 'break';
-      }
-    }
     const preparedQuestion = new Question(typeOfUserQuestion.typeOfQuestion, `question${indexOfQuestion}`, userQuestion.question);
 
-    if (preparedQuestion.type === 'list') {
+    if (typeOfUserQuestion.typeOfQuestion === 'list') {
+      const options = await generateOptions();
       preparedQuestion.choices = options;
     }
-    arr.push(preparedQuestion);
-    indexOfQuestion += 1;
 
-    const userWill = await inquirer.prompt({
-      type: 'input',
-      name: 'question',
-      message: 'Do you want set next question? (y|n)',
+    arr.push(preparedQuestion);
+    mode = await inquirer.prompt({
+      type: 'confirm',
+      name: 'value',
+      message: 'Do you want set next question?',
+      default: false,
     });
-    flowMode = userWill.question === 'y' ? 'next' : 'break';
+    mode = mode.value;
     console.clear();
   }
 };
